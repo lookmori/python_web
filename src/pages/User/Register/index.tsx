90ae4cfffcc50f715f667b9ee88e43ced19d9809
lookmori <58687559+lookmori@users.noreply.github.com>
2025-01-18 10:49:19 +0800

    修改注册页面

--- src/pages/User/Register/index.tsx
+++ src/pages/User/Register/index.tsx
@@ -1,23 +1,10 @@
+import { PageContainer, ProForm, ProFormCaptcha, ProFormText, ProFormTextArea } from '@ant-design/pro-components';
+import { Card, message } from 'antd';
 
-import {
-  PageContainer,
-  ProForm,
-  ProFormText,
-  ProFormTextArea,
-
-} from '@ant-design/pro-components';
-import {  Card, message } from 'antd';
-
-
-
-
-
-import React from 'react'
-
-import styles from './index.less'
-
+import styles from './index.less';
+import { MailTwoTone } from '@ant-design/icons';
+const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
 export default function RegisterUser() {
-
   return (
     <PageContainer>
       <div className={styles.container}>
@@ -31,32 +18,44 @@ export default function RegisterUser() {
               }}
               formKey="base-form-use-demo"
               autoFocusFirstInput
+              submitter={{
+                // 配置按钮的属性
+                resetButtonProps: {
+                  style: {
+                    display: 'none',
+                  },
+                },
+              }}
             >
               <ProFormText
-                width="lg"
+                width="xl"
+                name="user_nickname"
+                label="昵称"
+                rules={[{ required: true, message: '请输入昵称' }]}
+              />
+              <ProFormText
+                width="xl"
                 name="user_name"
-                label="用户名"
-                rules={[{ required: true, message: 'Please input your username!' }]}
+                label="用户名(登录账号)"
+                rules={[{ required: true, message: '请输入用户名' }]}
               />
               <ProFormText.Password
                 name="user_pwd"
-                width="lg"
+                width="xl"
                 label="密码"
-                rules={[{ required: true, message: 'Please input your username!' }]}
+                rules={[{ required: true, message: '请输入密码' }]}
               />
               <ProFormText.Password
                 name="user_sure_pwd"
-                width="lg"
+                width="xl"
                 label="确认密码"
                 dependencies={['user_pwd']}
                 rules={[
-                  { required: true, message: 'Please input your username!' },
+                  { required: true, message: '请输入确认密码' },
                   ({ getFieldValue }) => ({
                     validator(_, value) {
                       if (getFieldValue('user_pwd') !== value) {
-                        return Promise.reject(
-                          new Error('两次密码不一致，请检查！'),
-                        );
+                        return Promise.reject(new Error('两次密码不一致，请检查！'));
                       }
                     },
                   }),
@@ -64,9 +63,43 @@ export default function RegisterUser() {
               />
               <ProFormTextArea
                 name="user_des"
-                label="个人描述"
-                width="lg"
-                rules={[{ required: true, message: 'Please input your username!' }]}
+                label="个人名言"
+                width="xl"
+                rules={[{ required: true, message: '请输入个人名言' }]}
+              />
+              <ProFormCaptcha
+                width="md"
+                label="邮箱(找回密码使用)"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <MailTwoTone />,
+                }}
+                captchaProps={{
+                  size: 'large',
+                }}
+                // 手机号的 name，onGetCaptcha 会注入这个值
+                phoneName="email"
+                name="user_email"
+                rules={[
+                  {
+                    required: true,
+                    message: '请输入验证码',
+                  },
+                  () => ({
+                    validator(_, value) {
+                      if (emailPattern.test(value) ) {
+                        return Promise.resolve();
+                      }
+                      return Promise.reject(
+                        new Error('邮箱格式错误请检查！'),
+                      );
+                    },
+                  }),
+                ]}
+                placeholder="请输入验证码"
+                onGetCaptcha={async (email) => {
+                  message.success(`${email}的验证码发送成功!`);
+                }}
               />
             </ProForm>
           </Card>
@@ -74,4 +107,4 @@ export default function RegisterUser() {
       </div>
     </PageContainer>
   );
-};
+}
